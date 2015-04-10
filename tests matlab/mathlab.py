91bfc847d5da763b4e0c3ae6352297013e7d9ca2
Alexandre Brochu <alexbrochu1@gmail.com>
2015-04-10 12:14:08 -0400

    nouvelle verison du script mathlab.py qui calcule la transfomree de fourier pour la calcul de la note.

--- tests matlab/mathlab.py
+++ tests matlab/mathlab.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as num
 import pylab
 import matplotlib.pyplot as plt
@@ -6,43 +7,45 @@ import math
 
 from scipy import fft, arange
 
+THRESHOLD = 0.85
+
 def nextpow2(n):
-	exp2 = num.log2(n)
-	nextexp = num.ceil(exp2)
-	return nextexp
+    exp2 = num.log2(n)
+    nextexp = num.ceil(exp2)
+    return nextexp
 
 def getfftinfo(signal, drawGraph):
-	Fs = 44100
-	T = 1.0/Fs
-	L = len(signal)
+    Fs = 44100
+    T = 1.0/Fs
+    L = len(signal)
 
-	nfft = 2 ** nextpow2(L)
-	Y = fft(signal, int(nfft))
-	numUniquePts = num.ceil((nfft+1)/2)
-	Y = Y[1:numUniquePts]
-	mx = abs(Y)
-	mx = mx / L
-	mx = mx ** 2
+    nfft = 2 ** nextpow2(L)
+    Y = fft(signal, int(nfft))
+    numUniquePts = num.ceil((nfft+1)/2)
+    Y = Y[1:numUniquePts]
+    mx = abs(Y)
+    mx = mx / L
+    mx = mx ** 2
 
-	if nfft % 2:
-		mx[2:len(mx)] = mx[2:len(mx)]*2
-	else:
-		mx[2:len(mx) - 1] = mx[2:len(mx) - 1]*2
+    if nfft % 2:
+	mx[2:len(mx)] = mx[2:len(mx)]*2
+    else:
+	mx[2:len(mx) - 1] = mx[2:len(mx) - 1]*2
 
-	f = arange(numUniquePts - 1) * Fs / nfft
-	if drawGraph:
-		plt.plot(f, mx, 'r')
-		plt.xlabel('freq')
-		plt.ylabel('amp')
-		plt.show()
-	else:
-		return [f, mx]
+    f = arange(numUniquePts - 1) * Fs / nfft
+    if drawGraph:
+	plt.plot(f, mx, 'r')
+	plt.xlabel('freq')
+	plt.ylabel('amp')
+	plt.show()
+    else:
+    	return [f, mx]
 
 def findFreq(x, y):
-	threshold = 0.85 # Verifier si il y a un meilleur threashold
-	thresholdVal = num.max(y) * threshold
-	idx = num.where(y >= thresholdVal)
-	return x[idx]
+    threshold = THRESHOLD
+    thresholdVal = num.max(y) * threshold
+    idx = num.where(y >= thresholdVal)
+    return x[idx]
 
 def findNote(freqList):
     a = 2 ** (1.0 / 12.0)
@@ -50,13 +53,13 @@ def findNote(freqList):
     fn = freqList / f0
     notes = num.log(fn) / num.log(a)
     notes = num.round(notes)
-    print notes
+    return notes
 
-# Read sound file
-sampleRate, signal = wav.read('c.wav')
-print type(signal.tolist()[0])
-# get frequencies in signal
-x, y = getfftinfo(signal, False)
-freq = findFreq(x, y)
-print freq
-findNote(freq)
+if __name__ == '__main__':
+    # Read sound file
+    sampleRate, signal = wav.read(sys.argv[1])
+    # get frequencies in signal
+    x, y = getfftinfo(signal, False)
+    freq = findFreq(x, y)
+    print freq
+    print findNote(freq)
