3e6828388d39c4d4346f0e1647f90477f4418999
Alexandre Brochu <alexbrochu1@gmail.com>
2015-04-10 12:03:55 -0400

    fixed a reference to WIDTH constant, not used anymore

--- streamHandler.py
+++ streamHandler.py
@@ -12,7 +12,7 @@ DEFAULT_DEVICE_INDEX = 0
 DEFAULT_FRAMES_PER_BUFFER = 1024
 
 class streamInfo:
-    def __init__(self, width = DEFAULT_WIDTH, channels = DEFAULT_CHANNELS, rate = DEFAULT_RATE, deviceIndex = DEFAULT_DEVICE_INDEX, framesPerBuffer = DEFAULT_FRAMES_PER_BUFFER):
+    def __init__(self, width = 4, channels = DEFAULT_CHANNELS, rate = DEFAULT_RATE, deviceIndex = DEFAULT_DEVICE_INDEX, framesPerBuffer = DEFAULT_FRAMES_PER_BUFFER):
         self.width = width
         self.channels = channels
         self.rate = rate
@@ -25,26 +25,26 @@ class streamHandler:
             self.streaminfo = streamInfo()
         else:
             self.streaminfo = streaminfo
-            
+
         self.func = func
         self.outputGen = outputGen
         self._maxPrint = 100
         self._currentPrint = 0
         self.kkfini = False
-    
+
     def _getCallback(self):
         def _streamCallback(in_data, frame_count, time_info, status):
             if self._currentPrint < self._maxPrint:
                 self._currentPrint += 1
                 data = []
-                
+
                 data = numpy.fromstring(in_data, dtype=numpy.int16).tolist()
                 """
                 for i in range(frame_count):
                     width = self.streaminfo.width
                     temp = struct.unpack('h', '\x00' * (2 - width) + in_data[i*width : i*width + width])
                     data.append(temp[0] if temp[0] > CUTOFF_THRESHOLD else temp[0])"""
-                    
+
                 print "{0}: {1} frames using {2} bytes".format(time_info, frame_count, len(in_data))
                 print data
                 print ""
@@ -52,7 +52,7 @@ class streamHandler:
                 self.kkfini = True
             return (None, pyaudio.paContinue)
         return _streamCallback
-    
+
     def start(self):
         self._pa = pyaudio.PyAudio()
         self.stream = self._pa.open(format = pyaudio.paInt16,
@@ -62,12 +62,12 @@ class streamHandler:
                                     input_device_index = self.streaminfo.deviceIndex,
                                     frames_per_buffer = self.streaminfo.framesPerBuffer,
                                     stream_callback = self._getCallback())
-    
+
     def stop(self):
         self.stream.stop_stream()
         self.stream.close()
         self._pa.terminate()
-        
+
 if __name__ == "__main__":
     sh = streamHandler(None, None)
     sh.start()
