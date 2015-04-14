07aa6084e26640cd650cd0911001610c3cbe4bfd
Gabriel Marde <shitsuckass@fesses.tabarnakpenis.cul>
2015-04-14 13:32:18 -0400

    BaseStreamHandler pour le petit brochu

---
+++ baseStreamHandler.py
@@ -0,0 +1,36 @@
+# baseStreamHandler.py
+#------------------------
+# Abstract class representing stream handlers.
+
+import numpy
+
+from mathlab import fonctionReconnaissance
+from outputGenerator import outputGenerator
+
+DEFAULT_DATA_TYPE = numpy.int16
+DEFAULT_CHUNK_SIZE = 1024
+
+class baseStreamHandler:
+    def __init__(self, func = fonctionReconnaissance, outputGen = outputGenerator(), dataType = DEFAULT_DATA_TYPE, chunkSize = DEFAULT_CHUNK_SIZE):
+        self.func = func
+        self.outputGen = outputGen
+        self.dataType = dataType
+        self.chunkSize = chunkSize
+
+    def _getCallback(self):
+        def _streamCallback(in_data):
+            data = []
+            data = numpy.fromstring(in_data, dtype=self.dataType).tolist()
+            noteData = func(data)
+            
+            return _callBackProcessing(noteData)
+        return _streamCallback
+    
+    def _callBackProcessing(self, data):
+        self.outputGen.translate(data)
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
